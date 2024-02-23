869f44c702bca85a15348188e4f05f1f693c3123
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-02-23 17:51:17 +0000

    Passed props to Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,22 +1,27 @@
 import React from 'react';
 import '../styles/Header.css';
 
-const Header = () => {
+interface HeaderProps {
+  mode: string; 
+  toggleMode: () => void; 
+}
+
+const Header: React.FC<HeaderProps> = ({mode, toggleMode}) => {
   return (
-    <div className='Header'>
+    <div className={`Header ${mode}`}>
       <div>
         <h2> Choose your mode</h2>
-        <span className="material-symbols-outlined">
+        <span className="material-symbols-outlined" onClick={toggleMode}>
           arrow_forward
         </span>
       </div>
-      <span className="material-symbols-outlined">
+      <span className="material-symbols-outlined" onClick={toggleMode}>
         wb_sunny
       </span>
-      <span className="material-symbols-outlined">
+      <span className="material-symbols-outlined" onClick={toggleMode}>
         nightlight
       </span>
-      <span className="material-symbols-outlined">
+      <span className="material-symbols-outlined" onClick={toggleMode}>
         bolt
       </span>
     </div>
