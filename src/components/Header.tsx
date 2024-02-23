6ce7d2ff3b854f38a2770119dbf40fbfb33a33a0
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-02-23 17:37:28 +0000

    Updated Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React from 'react';
+import '../styles/Header.css';
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='Header'>
+      <div>
+        <h2> Choose your mode</h2>
+        <span className="material-symbols-outlined">
+          arrow_forward
+        </span>
+      </div>
+      <span className="material-symbols-outlined">
+        wb_sunny
+      </span>
+      <span className="material-symbols-outlined">
+        nightlight
+      </span>
+      <span className="material-symbols-outlined">
+        bolt
+      </span>
+    </div>
   )
 }
 
