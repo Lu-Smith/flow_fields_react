d9e0cc65e4aa4e9fe3ee0e79f2872cabae9bd03a
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-02-23 17:15:39 +0000

    Updated FooterComponent.tsx

--- src/components/FooterComponent.tsx
+++ src/components/FooterComponent.tsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React from 'react';
+import '../styles/Footer.css';
 
 const FooterComponent = () => {
   return (
-    <div>FooterComponent</div>
+    <div className='Footer'>
+        <h3>Coded by 
+            <a
+             href='https://www.lunasmithart.com/' 
+             target='_blank'
+             className='portfolio'>
+                Luna Smith
+            </a>
+            -
+            <a 
+            href='https://github.com/Lu-Smith/flow_fields_react'
+            target='_blank'>
+                GitHub
+            </a>
+            .
+        </h3>
+    </div>
   )
 }
 
