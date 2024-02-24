5e27e4e4df5fc880e8296705a6f68e3ef4bfec4f
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-02-24 14:26:47 +0000

    Updated App.tsx - added CanvasFour.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,18 @@ import MainCanvas from './components/MainCanvas';
 import CanvasOne from './components/CanvasOne';
 import CanvasTwo from './components/CanvasTwo';
 import CanvasThree from './components/CanvasThree';
+import { motion} from 'framer-motion';
+import CanvasFour from './components/CanvasFour';
+
+const childVariantsR = {
+  hidden: { x: -500, opacity: 0 },
+  visible: { x: 0, opacity: 1, transition: { delay: 0.2, duration: 1 } },
+};
+
+const childVariantsL = {
+  hidden: { x: 500, opacity: 0 },
+  visible: { x: 0, opacity: 1, transition: { delay: 0.2, duration: 1 } },
+};
 
 const App = () => {  
   const [mode, setMode] = useState('light');
@@ -20,12 +32,24 @@ const App = () => {
         <div className={`HeaderContainer ${mode}`}>
           <Header mode={mode} toggleMode={toggleMode} />
         </div>
-        <div className={`CanvasContainer ${mode}`}>
-          <MainCanvas mode={mode} />
-          <CanvasThree mode={mode}/>
-          <CanvasOne mode={mode}/>
-          <CanvasTwo mode={mode}/>
-        </div>
+        <motion.div 
+        className={`CanvasContainer ${mode}`}>
+          <motion.div variants={childVariantsR}  initial="hidden" whileInView={"visible"}>
+            <MainCanvas mode={mode} />
+          </motion.div>
+          <motion.div variants={childVariantsL} initial="hidden" whileInView={"visible"}>
+            <CanvasThree mode={mode} />
+          </motion.div>
+          <motion.div variants={childVariantsR} initial="hidden" whileInView={"visible"}>
+            <CanvasOne mode={mode} />
+          </motion.div>
+          <motion.div variants={childVariantsL} initial="hidden" whileInView={"visible"}>
+            <CanvasTwo mode={mode} />
+          </motion.div>
+          <motion.div variants={childVariantsL} initial="hidden" whileInView={"visible"}>
+            <CanvasFour mode={mode} />
+          </motion.div>
+        </motion.div>
         <div className={`FooterContainer ${mode}`}>
           <FooterComponent mode={mode} />
         </div>
